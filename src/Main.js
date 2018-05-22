f9636a1458cb91793d15b6361e08198e67f5ec62
Emma Nettleton <nettle10@msu.edu>
2018-05-22 14:45:16 -0400

    style main using jsx

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 import Sidebar from './Sidebar'
-
 import NoteList from './NoteList'
-
 import NoteForm from './NoteForm'
 
 
 const Main = () =>{
 return(
     <div className="Main">
+        className="Main"
+        style = {style}
        <Sidebar/>
        <NoteList/>
        <NoteForm/>
@@ -17,4 +17,9 @@ return(
 )
 }
 
+const style ={
+    color'read',
+    display:'flex',
+    alignItems: 'stretch',
+}
 export default Main
