c68cfa6c57ed083977e724331989f4ea91c67db7
Emma Nettleton <nettle10@msu.edu>
2018-05-22 15:10:38 -0400

    add images to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,23 @@
 import React from "react"
-
+import quill from './quill.spg'
+import newZoom from './new.png'
+import newHover from './new-hover.png'
 const Sidebar = () => {
     return(
-    <div className="Sidebar">
-        Sidebar
-        </div>
+        <nav className="Sidebar">
+          <div className="logo">
+            <img src={quill} alt="Noteherder"/>
+          </div>
+          <a className="new-note" href="/notes">
+            <img src={newHover} alt="New note"/>
+            <img className="outline" src={newZoom} alt="New note"/>
+          </a>
+          <div className="SignOut">
+            <button>
+              <i className="fa fa-sign-out"></i>
+            </button>
+          </div>
+        </nav>
     )
 }
 export default Sidebar
