5f14a2010673e58b2741197e662f9c7a2b194a91
Emma Nettleton <nettle10@msu.edu>
2018-05-23 14:06:02 -0400

    contuinue styling side bar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,42 +6,84 @@ import newHover from './media/new-hover.png'
 
 const Sidebar = () => {
   return (
-    <nav className="Sidebar" style ={styles.sidebar}>
-      <div className="logo" style = {styles.logo}>
-        <img src={quill} 
-        alt="Noteherder" 
-        style={styles.logoImg}/>
+    <nav className="Sidebar" 
+    style ={styles.sidebar}
+    >
+      <div 
+        className="logo" 
+        style = {styles.logo}
+      >
+        <img 
+          src={quill} 
+          alt="Noteherder" 
+          style={styles.logoImg}/>
       </div>
-      <a className="new-note" href="/notes">
-        <img src={newHover} alt="New note" />
-        <img className="outline" src={newIcon} alt="New note" />
+      <a 
+      className="new-note" 
+        href="/notes" 
+        style={styles.newNote}
+        >
+        <img 
+          src={newHover} 
+          alt="New note" 
+          style={styles.newNoteImg}/>
+        <img 
+        className="outline" 
+        src={newIcon} 
+        alt="New note" 
+        style={styles.newNoteImg}/>
       </a>
-      <div className="SignOut">
-        <button>
-          <i className="fa fa-sign-out"></i>
+      <div className="SignOut" style={styles.signOut}>
+        <button style={styles.button}>
+          <i className="fa fa-sign-out" style={styles.buttonIcon}></i>
         </button>
       </div>
     </nav>
   )
 }
-
-const styles = {
-    sidebar:{
-        width: '60px',
-        backgroundColor: '#f3f3f3',
-        padding: '0.5rem 0',
-        display: 'flex',
-        flexDirection: 'column', 
-        alignItems: 'center',
+    
+    const styles = {
+    sidebar: {
+      width: '6rem',
+      backgroundColor: '#f3f3f3',
+      padding: '0.5rem 0',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
-    logo:{
-        fontFamily: 'Fauna One',
-        color: '#666',
-        fontSize: '3rem',
+    logo: {
+      fontFamily: '"Fauna One"',
+      color: '#666',
+      fontSize: '3rem',
     },
     logoImg: {
-        width: '30px',
-        paddingLeft: '0.4rem',
-    }
-}
+      width: '3rem',
+      paddingLeft: '0.4rem',
+    },
+    newNote: {
+      marginTop: '2rem',
+      position: 'relative',
+      width: '4rem',
+    },
+    newNoteImg: {
+      position: 'absolute',
+      left: '0',
+      width: '100%',
+      transition: 'opacity 0.25s ease-in-out',
+    },
+    signOut: {
+      position: 'absolute',
+      bottom: '1rem',
+    },
+    button: {
+      backgroundColor: 'transparent',
+      border: '0',
+      color: '#008bf8',
+      cursor: 'pointer',
+      outline: 'none',
+    },
+    buttonIcon: {
+      fontSize: '2rem',
+    },
+  }
 export default Sidebar
